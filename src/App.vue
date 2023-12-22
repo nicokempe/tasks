68b77be0ed5c1eb06b09d271c3a8fa815828a54f
Nico Kempe <accounts@nicokempe.de>
2023-12-22 17:45:12 +0100

    Initialize Vue 3 project
    * Create Vue 3 project
    * Add E2E testing dependency (Cypress)
    * Add unit test dependency (Jest)
    * Add Eslint
    * Add Babel
    * Add TailwindCSS
    * Add TypeScript

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <!-- ToDo app components will go here -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    // Import and register components
+  },
+});
+</script>
+
+<style scoped>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+</style>
