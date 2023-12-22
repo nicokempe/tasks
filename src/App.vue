28338f67d7e06037014090f5136c1f12141f50d5
Nico Kempe <accounts@nicokempe.de>
2023-12-22 18:23:36 +0100

    Integrate the components into the App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="app-container">
-    <!-- ToDo app components will go here -->
+    <task-input @addTask="addTask" />
+    <task-list :tasks="tasks" @toggleTask="toggleTask" @removeTask="removeTask" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import TaskInput from './components/TaskInput.vue';
+import TaskList from './components/TaskList.vue';
+import { ToDoItem } from './models/ToDoItem';
 
 export default defineComponent({
   name: 'App',
   components: {
-    // Import and register components
+    TaskInput,
+    TaskList,
+  },
+  setup() {
+    const tasks = ref<ToDoItem[]>([]);
+
+    const saveTasks = () => {
+      localStorage.setItem('tasks', JSON.stringify(tasks.value));
+    };
+
+    const addTask = (title: string) => {
+      const newTask = {
+        id: Date.now(),
+        title,
+        completed: false,
+      };
+      tasks.value.push(newTask);
+      saveTasks();
+    };
+
+    const toggleTask = (id: number) => {
+      const taskToToggle = tasks.value.find((t) => t.id === id);
+      if (taskToToggle) {
+        taskToToggle.completed = !taskToToggle.completed;
+        saveTasks();
+      }
+    };
+
+    const removeTask = (id: number) => {
+      tasks.value = tasks.value.filter((t) => t.id !== id);
+      saveTasks();
+    };
+
+    onMounted(() => {
+      const storedTasks = localStorage.getItem('tasks');
+      if (storedTasks) {
+        tasks.value = JSON.parse(storedTasks);
+      }
+    });
+
+    return {
+      tasks,
+      addTask,
+      toggleTask,
+      removeTask,
+    };
   },
 });
 </script>
